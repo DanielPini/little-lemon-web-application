61665d597675ffd18970bc7d0a88a155ca31f154
DanielPini <daniel@danielpini.com>
2026-03-27 12:50:38 +0100

    Add occasion to booking form

--- src/components/reservations/Reservations.jsx
+++ src/components/reservations/Reservations.jsx
@@ -7,6 +7,23 @@ function Reservations() {
         <h1>Book a table</h1>
         <div className="book-image"></div>
       </section>
+      <section className="booking-form">
+        <form action="post">
+          <fieldset>
+            <label htmlFor="select-occasion">
+              Select Occasion
+              <select name="select-occasion" id="select-occasion">
+                <option value="birthday">Birthday</option>
+                <option value="anniversary">Anniversary</option>
+                <option value="engagement">Engagement</option>
+                <option value="meeting">Business Meeting</option>
+                <option value="other">Other</option>
+              </select>
+            </label>
+            <label htmlFor="">Enter your information</label>
+          </fieldset>
+        </form>
+      </section>
     </>
   );
 }
